4a78552d62d4f51358803573fee6f416288c4b57
You <you@example.com>
2024-01-15 09:23:00 +0000

    Add basic task list

--- TaskApp/Program.cs
+++ TaskApp/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+var tasks = new List<string>
+{
+    "Buy milk",
+    "Write report",
+    "Call Alice",
+    "Fix bug"
+};
+
+Console.WriteLine("Tasks:");
+foreach (var task in tasks)
+{
+    Console.WriteLine(task);
+}
